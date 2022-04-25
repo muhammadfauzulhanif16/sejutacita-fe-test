5c02cb92c424f87691f2672f4acd02bcb5ddda83
muhammadfauzulhanif16 <muhammadfauzulhanif16@outlook.com>
2022-04-26 05:48:16 +0700

    Create library page

---
+++ src/pages/index.tsx
@@ -0,0 +1,38 @@
+import type { NextPage } from "next";
+import { useState } from "react";
+import { useReadBooksPerpageQuery } from "../app/services/bookApi";
+import { Shelf } from "../components/Shelf";
+
+const Home: NextPage = () => {
+  const [categoryId, setCategoryId] = useState<string>(""),
+    [page, setPage] = useState<string>("1");
+
+  const {
+    data: books = [],
+    isSuccess: isSuccessBooks,
+    isLoading: isLoadingBooks,
+    isFetching: isFetchingBooks,
+  } = useReadBooksPerpageQuery({ categoryId, page });
+
+  const handleSelect = ({ target: { value } }: any) => {
+    setPage("");
+    setCategoryId(String(value));
+  };
+
+  return (
+    <Shelf
+      titlePage="Library"
+      descriptionPage="All collections of interesting books"
+      data={books}
+      isSuccessBooks={isSuccessBooks}
+      isLoadingBooks={isLoadingBooks}
+      isFetchingBooks={isFetchingBooks}
+      handleSelect={handleSelect}
+      categoryId={categoryId}
+      page={page}
+      setPage={setPage}
+    />
+  );
+};
+
+export default Home;
